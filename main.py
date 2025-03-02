d5bc537fc4f06c60dbe203e858f954f89dcd9e59
silvermete0r <gregormelon.2003@gmail.com>
2025-03-02 13:08:19 +0500

    final-updates

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ def hash_username(username):
     return hashlib.md5(username.encode("utf-8")).hexdigest()
 
 # Shields.io Badge Template
-BADGE_TEMPLATE = '![DeepML {display_username}](https://img.shields.io/badge/dynamic/json?url=https%3A%2F%2Fraw.githubusercontent.com%2Fsilvermete0r%2Fdeepml-top%2Frefs%2Fheads%2Fmain%2Fbadges.json&query=%24.{encoded_key}.label&prefix=Rank%20&style=for-the-badge&label=%F0%9F%9A%80%20DeepML&color=blue&link=https%3A%2F%2Fwww.deep-ml.com%2Fleaderboard)'
+BADGE_TEMPLATE = '![DeepML {display_username}](https://img.shields.io/badge/dynamic/json?url=url=https%3A%2F%2Fraw.githubusercontent.com%2Fsilvermete0r%2Fdeepml-top%2Fmain%2Fbadges.json&query=%24.{encoded_key}.label&prefix=Rank%20&style=for-the-badge&label=%F0%9F%9A%80%20DeepML&color=blue&link=https%3A%2F%2Fwww.deep-ml.com%2Fleaderboard)'
 
 # Initialize Selenium WebDriver
 options = webdriver.ChromeOptions()
@@ -142,7 +142,7 @@ def save_json(leaderboard):
     for rank, username, score in leaderboard:
         encoded_username = hash_username(username)
         badges[encoded_username] = {
-            "label": f"No. {rank} {username}",
+            "label": f"No. {rank} | {username}",
             "rank": rank,
             "score": score,
             "username": username
