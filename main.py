5dbaddb4703ead755044ce549f8fdf76cab64996
silvermete0r <gregormelon.2003@gmail.com>
2025-03-02 12:59:26 +0500

    md5-only-letters-alpha

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ from selenium.webdriver.support import expected_conditions as EC
 from bs4 import BeautifulSoup
 import json
 import hashlib
+import string  
 
 # GitHub File Paths
 README_FILE = "README.MD"
@@ -17,7 +18,15 @@ BADGES_JSON = "badges.json"
 
 # Hash username for badge template
 def hash_username(username):
-    return hashlib.md5(username.encode("utf-8")).hexdigest()
+    md5_hash = hashlib.md5(username.encode("utf-8")).hexdigest()
+    num = int(md5_hash, 16)
+    letters = string.ascii_lowercase
+    base = len(letters)
+    result = ""
+    while num:
+        num, rem = divmod(num, base)
+        result = letters[rem] + result
+    return result
 
 # Shields.io Badge Template
 BADGE_TEMPLATE = '![DeepML {display_username}](https://img.shields.io/badge/dynamic/json?url=https%3A%2F%2Fraw.githubusercontent.com%2Fsilvermete0r%2Fdeepml-top%2Frefs%2Fheads%2Fmain%2Fbadges.json&query=%24.{encoded_key}.label&prefix=Rank%20&style=for-the-badge&label=%F0%9F%9A%80%20DeepML&color=blue&link=https%3A%2F%2Fwww.deep-ml.com%2Fleaderboard)'
